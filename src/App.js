bd5a53e8042893ce81c92a72f867f3ff612d2ee6
doggybay <julio.miranda12@gmail.com>
2020-01-29 21:59:54 +0000

    fixed issues

--- src/App.js
+++ src/App.js
@@ -6,51 +6,50 @@ function App() {
   let _scannerIsRunning = false;
 
   const startScanner = () => {
-    Quagga.init({
-
-      inputStream: {
-
-        name: "Live",
-        type: "LiveStream",
-        target: document.querySelector('#scanner-container'),
-        constraints: {
+    Quagga.init(
+      {
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          target: document.querySelector("#scanner-container"),
+          constraints: {
             width: 480,
             height: 320,
             facingMode: "environment"
-        },
-      },
-      decoder: {
-          readers: [
-              "code_128_reader",
-              "ean_reader",
-              "ean_8_reader",
-              "code_39_reader",
-              "code_39_vin_reader",
-              "codabar_reader",
-              "upc_reader",
-              "upc_e_reader",
-              "i2of5_reader"
-          ],
-          debug: {
-              showCanvas: true,
-              showPatches: true,
-              showFoundPatches: true,
-              showSkeleton: true,
-              showLabels: true,
-              showPatchLabels: true,
-              showRemainingPatchLabels: true,
-              boxFromPatches: {
-                  showTransformed: true,
-                  showTransformedBox: true,
-                  showBB: true
-              }
           }
         },
-
-    }, (err) => {
+        decoder: {
+          readers: [
+            "code_128_reader",
+            "ean_reader",
+            "ean_8_reader",
+            "code_39_reader",
+            "code_39_vin_reader",
+            "codabar_reader",
+            "upc_reader",
+            "upc_e_reader",
+            "i2of5_reader"
+          ],
+          debug: {
+            showCanvas: true,
+            showPatches: true,
+            showFoundPatches: true,
+            showSkeleton: true,
+            showLabels: true,
+            showPatchLabels: true,
+            showRemainingPatchLabels: true,
+            boxFromPatches: {
+              showTransformed: true,
+              showTransformedBox: true,
+              showBB: true
+            }
+          }
+        }
+      },
+      err => {
         if (err) {
-            console.log(err);
-            return
+          console.log(err);
+          return;
         }
 
         console.log("Initialization finished. Ready to start");
@@ -58,38 +57,58 @@ function App() {
 
         // Set flag to is running
         _scannerIsRunning = true;
-    });
+      }
+    );
 
-    Quagga.onProcessed((result) => {
-
+    Quagga.onProcessed(result => {
       let drawingCtx = Quagga.canvas.ctx.overlay;
       let drawingCanvas = Quagga.canvas.dom.overlay;
 
       if (result) {
         if (result.boxes) {
-            drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-            result.boxes.filter((box) => {
-                return box !== result.box;
-            }).forEach((box) => {
-              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
+          drawingCtx.clearRect(
+            0,
+            0,
+            parseInt(drawingCanvas.getAttribute("width")),
+            parseInt(drawingCanvas.getAttribute("height"))
+          );
+          result.boxes
+            .filter(box => {
+              return box !== result.box;
+            })
+            .forEach(box => {
+              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+                color: "green",
+                lineWidth: 2
+              });
             });
         }
 
         if (result.box) {
-            Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
+          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+            color: "#00F",
+            lineWidth: 2
+          });
         }
 
         if (result.codeResult && result.codeResult.code) {
-            Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+          Quagga.ImageDebug.drawPath(
+            result.line,
+            { x: "x", y: "y" },
+            drawingCtx,
+            { color: "red", lineWidth: 3 }
+          );
         }
       }
     });
 
-
-    Quagga.onDetected(function (result) {
-        console.log("Barcode detected and processed : [" + result.codeResult.code + "]", result);
+    Quagga.onDetected(function(result) {
+      console.log(
+        "Barcode detected and processed : [" + result.codeResult.code + "]",
+        result
+      );
     });
-  }
+  };
 
   const handleClick = () => {
     if (_scannerIsRunning) {
@@ -97,14 +116,16 @@ function App() {
     } else {
       startScanner();
     }
-  }
+  };
 
 
   
   return (
     <div className="App">
       <div id="scanner-container"></div>
-      <button id="btn" onClick={handleClick} value="Start/Stop the scanner" />
+      <button id="btn" onClick={handleClick} value="Start/Stop the scanner">
+        Start/stop scanner
+      </button>
     </div>
   );
 }
