ed7cd23d2fc9a11acf808297e8eccf4442cafa0d
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2020-01-29 13:13:44 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,110 @@
 import React from 'react';
-import logo from './logo.svg';
+import Quagga from 'quagga';
 import './App.css';
 
 function App() {
+  let _scannerIsRunning = false;
+
+  const startScanner = () => {
+    Quagga.init({
+
+      inputStream: {
+
+        name: "Live",
+        type: "LiveStream",
+        target: document.querySelector('#scanner-container'),
+        constraints: {
+            width: 480,
+            height: 320,
+            facingMode: "environment"
+        },
+      },
+      decoder: {
+          readers: [
+              "code_128_reader",
+              "ean_reader",
+              "ean_8_reader",
+              "code_39_reader",
+              "code_39_vin_reader",
+              "codabar_reader",
+              "upc_reader",
+              "upc_e_reader",
+              "i2of5_reader"
+          ],
+          debug: {
+              showCanvas: true,
+              showPatches: true,
+              showFoundPatches: true,
+              showSkeleton: true,
+              showLabels: true,
+              showPatchLabels: true,
+              showRemainingPatchLabels: true,
+              boxFromPatches: {
+                  showTransformed: true,
+                  showTransformedBox: true,
+                  showBB: true
+              }
+          }
+        },
+
+    }, (err) => {
+        if (err) {
+            console.log(err);
+            return
+        }
+
+        console.log("Initialization finished. Ready to start");
+        Quagga.start();
+
+        // Set flag to is running
+        _scannerIsRunning = true;
+    });
+
+    Quagga.onProcessed((result) => {
+
+      let drawingCtx = Quagga.canvas.ctx.overlay;
+      let drawingCanvas = Quagga.canvas.dom.overlay;
+
+      if (result) {
+        if (result.boxes) {
+            drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+            result.boxes.filter((box) => {
+                return box !== result.box;
+            }).forEach((box) => {
+              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
+            });
+        }
+
+        if (result.box) {
+            Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
+        }
+
+        if (result.codeResult && result.codeResult.code) {
+            Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+        }
+      }
+    });
+
+
+    Quagga.onDetected(function (result) {
+        console.log("Barcode detected and processed : [" + result.codeResult.code + "]", result);
+    });
+  }
+
+  const handleClick = () => {
+    if (_scannerIsRunning) {
+      Quagga.stop();
+    } else {
+      startScanner();
+    }
+  }
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="scanner-container"></div>
+      <button id="btn" onClick={handleClick} value="Start/Stop the scanner" />
     </div>
   );
 }
